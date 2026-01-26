c9ff4db7c56b25c1913398b64e289b26d244f3c8
zisanahmed97 <zisanahmed97@gmail.com>
2026-01-26 21:04:06 +0600

    created js-loop folder and added for-of.js file

---
+++ for-of.js
@@ -0,0 +1,16 @@
+const numbers = [12, 34, 56, 78, 90];
+
+// console.log(numbers);
+
+// for(const number of numbers){
+//     console.log(number);
+// }
+
+
+
+const fruits=['mango','orange','lichi','jackfruit'];
+
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log("i want to eat");
+}
